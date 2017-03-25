1d10e4a04d8a2d0f1cd8c2c4b1a6626d4baa63f4
Vu Tran <vu@vu-tran.com>
2017-03-25 00:15:07 -0700

    add support for promises

--- cli.es6.js
+++ cli.es6.js
@@ -1,22 +1,48 @@
+const https = require('https');
 const minimist = require('minimist');
 const pkg = require('./package');
 const Recon = require('./');
 
-// actions
-const getDescription = (props) => {
-    return pkg.description;
-};
+// helpers
+const searchRepo = (q) => new Promise((resolve, reject) => {
+    const url = `https://api.github.com/search/repositories?q=${q}`;
+    const opts = {
+        headers: {
+            'User-Agent': 'recon-demo',
+        },
+        hostname: 'api.github.com',
+        path: `/search/repositories?q=${q}`,
+    };
+    https.get(opts, res => {
+        let data = '';
+        res
+            .on('data', d => {
+                data += d;
+            })
+            .on('end', () => {
+                let d = JSON.parse(data);
+                resolve(d);
+            });
+    });
+});
 
-const getVersion = (props) => {
-    return pkg.version;
-};
 
+// search
+const search = (args) => {
+    const q = args[1];
+    return searchRepo(q)
+        .then(res => res.items.map(item => item.full_name))
+        .then(repos => {
+            let log = 'Found: \n';
+            log += repos.join('\n');
+            return log;
+        });
+}
 
 // creates the program
 const Program = (props) => (
-    <program input={props.input._[0]}>
-        <command name="help" onAction={getDescription} />
-        <command name="version" onAction={getVersion} />
+    <program input={props.input}>
+        <command name="search" onAction={search} />
     </program>
 );
 
